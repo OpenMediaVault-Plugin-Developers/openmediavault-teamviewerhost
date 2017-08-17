f5807d15d3b2617a229087d4787db097e871a0b9
Aaron Murray <plugins@omv-extras.org>
2017-08-16 19:11:54 -0500

    initial commit

---
+++ var/www/openmediavault/js/omv/module/admin/service/teamviewerhost/Settings.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2017 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/form/Panel.js")
+// require("js/omv/window/MessageBox.js")
+// require("js/omv/module/admin/service/teamviewerhost/Password.js")
+
+Ext.define('OMV.module.admin.service.teamviewerhost.Settings', {
+    extend: 'OMV.workspace.form.Panel',
+    requires: [
+        'OMV.module.admin.service.teamviewerhost.Password'
+    ],
+
+    rpcService: 'TeamviewerHost',
+    rpcGetMethod: 'getSettings',
+    rpcSetMethod: 'setSettings',
+
+    getButtonItems: function() {
+        var items = this.callParent(arguments);
+
+        items.push({
+            id: this.getId() + '-reset-password',
+            xtype: 'button',
+            text: _('Reset Password'),
+            icon: 'images/wrench.png',
+            iconCls: Ext.baseCSSPrefix + 'btn-icon-16x16',
+            scope: this,
+            handler: Ext.Function.bind(this.onResetPasswordButton, this)
+        });
+
+        return items;
+    },
+
+    getFormItems: function() {
+        return [{
+            xtype: 'fieldset',
+            title: 'General settings',
+            defaults: {
+                labelSeparator: ''
+            },
+            items: [{
+                xtype: 'checkbox',
+                name: 'enable',
+                fieldLabel: _('Enable'),
+                checked: false
+        }];
+    },
+
+    onResetPasswordButton: function() {
+        Ext.create('OMV.module.admin.service.teamviewerhost.Password', {
+            title: _('Reset password.'),
+            mode: 'remote',
+            rpcService: 'TeamviewerHost',
+            rpcSetMethod: 'doResetPassword'
+        }).show();
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id: 'settings',
+    path: '/service/teamviewerhost',
+    text: _('Settings'),
+    position: 10,
+    className: 'OMV.module.admin.service.teamviewerhost.Settings'
+});
